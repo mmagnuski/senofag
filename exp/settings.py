bbc67a5ee742798803c16d4b4e815a100501d5ae
mmagnuski <mmagnuski@swps.edu.pl>
2017-07-09 16:48:47 +0200

    STY & FIX: style changes and fix to eval_resp

--- exp/settings.py
+++ exp/settings.py
@@ -13,24 +13,25 @@ exp = dict()
 exp['kwadrat'] = Kwadrat
 exp['buttons'] = ['l', 'd']
 exp['block trials'] = 72 # actually, jest 48 przy obecnym prop
-exp['proportions'] = [(9,3), (3,9)] # can change
+exp['proportions'] = [(9, 3), (3, 9)] # can change
 
-# experimental basic deaths (defs):
+
+# experimental basic defs:
 def shuffle_colors(stim):
-	Kolory = []
-	Kolory.append(stim['circle']['red'])
-	Kolory.append(stim['circle']['green'])
-	Kolory.append(stim['circle']['yellow'])
-	Kolory.append(stim['circle']['blue'])
+	Kolory = list()
+	for color in ['red', 'green', 'yellow', 'blue']:
+		Kolory.append(stim['circle'][color])
 
 	# Randomize array Kolory:
 	random.shuffle(Kolory)
 	return Kolory
 
+
 def get_colors_from_square(colors, N, exp=exp):
 	names = ['lc', 'li', 'rc', 'ri']
 	return {cnd: colors[exp['kwadrat'][N, i]]
-		for i, cnd in enumerate(names)}
+			for i, cnd in enumerate(names)}
+
 
 def shuffle_rows(df):
 	df = df.reset_index(drop=True)
@@ -41,7 +42,10 @@ def shuffle_rows(df):
 	df.index = df.index + 1
 	return df
 
+
 def get_block(blockNum, prop=0, exp=exp):
+	'''What does it do? Seems it creates block, not just gets it'''
+
 	columns=['block', 'cond', 'choiceType', 'fixTime', 'prime', 'target',
 		'effect', 'pos', 'corrResp', 'resp', 'ifcorr']
 	dtp = ['int32', 'category', 'category', 'int32', 'category', 'category',
@@ -52,13 +56,14 @@ def get_block(blockNum, prop=0, exp=exp):
 	template = template.loc[:, temp_cols]
 
 	# content
-	# CHANGE prop to specific 
+	# what is prop? - proportion of trials of given type?
+	# CHANGE prop to specific
 	prop = random.choice(exp['proportions'])
 	df = pd.concat([template.query('choiceType == "Cued"')] * prop[0] +
 		[template.query('choiceType == "Free"')] * prop[1])
 	df = shuffle_rows(df)
 
-	# add missing rows
+	# add missing rows - what kind of missing rows?
 	temp_cols = [c for c in columns if c not in template.columns]
 	for c in temp_cols:
 		df[c] = 0
--- exp/stim.py
+++ exp/stim.py
@@ -1,18 +1,16 @@
-from psychopy import core, visual, event, monitors
-import numpy as np
+import os
 from os import path as op
 
-####################################################
-
-stim_dir = lambda pth : op.join('pic', pth)
+import numpy as np
+from psychopy import core, visual, event, monitors
 
 win = visual.Window((1200,1000),
     fullscr=False, monitor='testMonitor',
     units='deg', color='black') # MUST BE CHANGED (E.G. TO A DEFAULT ONE)
 
 def circle(win, col='green', pos=(0,0), r=2.5):
-    circ = visual.Circle(win, pos=pos, radius=r, edges=128,
-      units='deg', interpolate=True)
+    circ = visual.Circle(win, pos=pos, radius=r, edges=128, units='deg',
+                         interpolate=True)
     circ.setFillColor(col)
     circ.setLineColor(col)
     return circ
@@ -38,36 +36,25 @@ colors = ['grey', 'blue', 'red', 'green', 'yellow']
 stim['circle'] = {c: circle(win, col=c) for c in colors}
 stim['circle']['cross'] = visual.ImageStim(win, image=stim_dir('cross.png'))
 
-def whiteshape(v, win = win):      
-    return visual.ShapeStim(win,        
-       lineWidth  = 0.5,
-       fillColor  = [1, 1, 1],     
-       lineColor  = [1, 1, 1],     
-       vertices   = v,
-       closeShape = True
-       )
 
-# create fixation cross:       
-def fix(ln=1, lw=0.1):
-    hlw = lw/2
-    v = np.array([
-               [hlw, -ln],
-               [hlw, ln],
-               [-hlw, ln],
-               [-hlw, -ln] ])
+def whiteshape(v, win=None):
+    return visual.ShapeStim(win, lineWidth=0.5, fillColor=[1, 1, 1],
+                            lineColor=[1, 1, 1], vertices=v, closeShape=True)
 
-    fix = []
-    fix.append(whiteshape(v))
-    fix.append(whiteshape(      
-       np.fliplr(v)        
-       ))      
+# create fixation cross:
+def fix(ln=1, lw=0.1, win=None):
+    hlw = lw / 2
+    v = np.array([[hlw, -ln], [hlw, ln], [-hlw, ln], [-hlw, -ln]])
+    fix = list()
+    fix.append(whiteshape(v, win=win))
+    fix.append(whiteshape(np.fliplr(v), win=win))
     return fix
 
 stim['fix'] = fix()
 
-def show_trial(df, stim, tri):
+def show_trial(df, stim, trial, effect_colors=None):
     # show fixation
-    fix_frames = df.loc[tri, 'fixTime']
+    fix_frames = df.loc[trial, 'fixTime']
     for f in stim['fix']:
         f.autoDraw = True
 
@@ -75,19 +62,19 @@ def show_trial(df, stim, tri):
         stim['win'].flip()
 
     # show prime
-    stim[df.loc[tri, 'prime']].draw()
+    stim[df.loc[trial, 'prime']].draw()
     stim['win'].flip()
 
     # show target
     event.getKeys()
     for _ in range(25):
-        stim[df.loc[tri, 'target']].draw()
+        stim[df.loc[trial, 'target']].draw()
         stim['win'].flip()
 
     # 1500 ms for response
     for _ in range(150):
-        k = event.getKeys()
-        if k:
+        key = event.getKeys()
+        if key:
             break
         stim['win'].flip()
 
@@ -95,21 +82,24 @@ def show_trial(df, stim, tri):
         f.autoDraw = False
 
     # evaluate repsonse
-    eval_resp(df, tri, k)
+    eval_resp(df, trial, key, effect_colors=effect_colors)
 
     for _ in range(30):
-        stim['circle'][df.loc[tri, 'effect']].draw()
+        stim['circle'][df.loc[trial, 'effect']].draw()
         stim['win'].flip()
-        
-def eval_resp(df, t, k):
-  if len(k) == 0:
-    k = 'NoResp'
-    df.loc[t, 'effect'] = 'cross'
-    df.loc[t, 'resp'] = k
-  else:
-    df.loc[t, 'resp'] = k[0]
-    df.loc[t, 'ifcorr'] = k[0] in df.loc[t, 'corrResp']
-    if not df.loc[t, 'ifcorr']:
-      df.loc[t, 'effect'] = 'cross'
 
 
+def eval_resp(df, trial, key, effect_colors=None):
+    if len(key) == 0:
+        key = 'NoResp'
+        df.loc[trial, 'effect'] = 'cross'
+        df.loc[trial, 'resp'] = key
+    else:
+        df.loc[trial, 'resp'] = key[0]
+        df.loc[trial, 'ifcorr'] = key[0] in df.loc[trial, 'corrResp']
+        if not df.loc[trial, 'ifcorr']:
+            df.loc[trial, 'effect'] = 'cross'
+        else:
+            used_hand = 'l' if key == 'd' else 'r'
+            condition = 'c' if used_hand == df.loc[trial, 'prime'][6] else 'i'
+            df.loc[trial, 'effect'] = effect_colors[used_hand + condition]
