e26f3d4d5920ae150d09f7ca821049da9868e6f7
mmagnuski <mmagnuski@swps.edu.pl>
2017-07-09 16:56:49 +0200

    change main

--- exp/main.py
+++ exp/main.py
@@ -1,11 +1,13 @@
 import settings as st
-from stim import stim
+from stim import create_stimuli, show_trial
 
 blck = st.get_block(0)
-colors = st.shuffle_colors(stim)
+stim = create_stimuli()
+# colors = st.shuffle_colors(stim)
 
 N = 0
-cond2color = get_colors_from_square(colors, N)
+colors = ['red', 'green', 'yellow', 'blue']
+cond_color = st.get_colors_from_square(colors, N)
 
 for t in range(1, 4):
-	stim.show_trial(blck, stim.stim, t)
+	show_trial(blck, stim, t, effect_colors=cond_color)
