d58c23262d35ad0e82f2d5dbd73bef36eed34318
leontikos <leontikos@gmail.com>
2018-01-30 18:55:16 +0100

    Add dialog box to main.py

--- exp/main.py
+++ exp/main.py
@@ -1,6 +1,16 @@
 import os
 from settings import create_settings, create_block, get_colors_from_square
 from stim import create_stimuli, show_trial, Instructions
+from psychopy import core, gui
+
+myDlg = gui.Dlg(title="Senofag")
+myDlg.addText('Subject info')
+myDlg.addField('ID:')
+ok_data = myDlg.show()
+if myDlg.OK:  # or if ok_data is not None
+    print(ok_data)
+else:
+    core.quit()
 
 settings = create_settings()
 stim = create_stimuli(fullscr=True)
@@ -23,8 +33,8 @@ block_number = 0
 blck = create_block(block_number, settings=settings)
 colors = ['red', 'green', 'yellow', 'blue']
 cond_color = get_colors_from_square(colors, block_number, settings=settings)
-data_fname = 'test_subject_block_{}.csv'.format(block_number)
+data_fname = '{}_block_{}.csv'.format(myDlg.data[0], block_number)
 
-for trial in range(1, 6):
+for trial in range(1, 9):
 	show_trial(blck, stim, trial, effect_colors=cond_color)
 	blck.to_csv(os.path.join(settings['data dir'], data_fname))
