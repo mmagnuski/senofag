8dd34f7c475be5f57384e60e7bee45ce1a8e1a4f
mmagnuski <mmagnuski@swps.edu.pl>
2018-03-24 21:22:22 +0100

    other changes

--- exp/test_razor_shape.py
+++ exp/test_razor_shape.py
@@ -10,13 +10,7 @@ from stim import subject_id_gui, create_stimuli, run_block, ArrowStim
 
 colors = ['red', 'green', 'yellow', 'blue']
 settings = create_settings(short_test=False, send_triggers=False)
-stim = create_stimuli(fullscr=True, settings=settings)
-
-#arrow = ArrowStim(stim['win'])
-#arrow.draw()
-#stim['win'].flip()
-#event.waitKeys()
-
+stim = create_stimuli(fullscr=True, settings=settings, monitor='lab')
 
 # TODO change arrow arrow_sharpness, color & magnitude! approx. 3.8 degrees,
 # 60 cm away from the screen
@@ -27,15 +21,17 @@ arrow.draw()
 stim['win'].flip()
 event.waitKeys()
 
-#arrow = ArrowStim(stim['win'], razor_width=2., arrow_width=3.,
-#                  arrow_sharpness=2.)
-#arrow.draw()
-#stim['win'].flip()
-#event.waitKeys()
+arrow.razor.setLineColor('red')
+arrow.razor.setFillColor('pink')
+arrow.arrow.draw()
+stim['win'].flip()
+event.waitKeys()
 
-#arrow = ArrowStim(stim['win'], razor_width=2., arrow_width=3.,
-#                  arrow_sharpness=2., razor_sharpness=0.5)
-#arrow.setPos((0., 3.))
-#arrow.draw()
-#stim['win'].flip()
-#event.waitKeys()
+arrow.draw()
+stim['win'].flip()
+event.waitKeys()
+
+arrow.setPos((0., 3.))
+arrow.draw()
+stim['win'].flip()
+event.waitKeys()
